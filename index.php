7158dab6d7418bc67912506d5e481a35370ff93f
Gerald Burke <burkeg@goldmail.etsu.edu>
2020-11-08 20:56:15 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,88 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+$file = json_decode(file_get_contents("json/tarot_card_data.json"));
+
+$cards = $file->cards;
+
+// Iterator for card id's
+$iterator = 1;
+// Attach image url to each card
+foreach($cards as $card) {
+  $card->imageURL = "https://indiealchemy.com/apis/plateautarot/images/".$card->name.".jpg";
+  $card->id = $iterator;
+  $iterator++;
+}
+
+// Apply Fisher-Yates shuffle to deck
+for($i = 0; $i < sizeof($cards); ++$i) {
+   $r = rand(0, $i);
+   $tmp = $cards[$i];
+   $cards[$i] = $cards[$r];
+   $cards[$r] = $tmp;  
+}
+
+// Returns card with matching id
+if (isset($_GET["id"])) {
+  foreach ($cards as $card) {
+    if ($card->id == $_GET["id"]){
+      echo json_encode($card);
+      die();
+    }
+  }
+  echo "Card not found";
+  die();
+}
+
+// Returns all cards of matching arcana
+if (isset($_GET["arcana"])) {
+    $selection = [];
+    foreach ($cards as $card) {
+        if (strtolower($card->arcana) == strtolower($_GET["arcana"])) {
+            array_push($selection, $card);
+        }
+    }
+    if (count($selection) < 1) {
+        echo "Invalid Arcana";
+        die();
+    }
+    $cards = $selection;
+}
+
+// Returns all cards of matching suit
+if (isset($_GET["suit"])) {
+    $selection = [];
+    foreach ($cards as $card) {
+        if (strtolower($card->suit) == strtolower($_GET["suit"])) {
+            array_push($selection, $card);
+        }
+    }
+    if (count($selection) < 1) {
+        echo "Invalid Suit";
+        die();
+    }
+    $cards = $selection;
+}
+
+// Returns n number of cards
+if (isset($_GET["count"])) {
+    try {
+        if ($_GET["count"] > 78 || $_GET["count"] < 1) {
+            echo "Invalid Number";
+            die();
+        }
+        $selection = [];
+        for ($i = 0; $i < $_GET["count"]; $i++) {
+            array_push($selection, $cards[$i]);
+        }
+        $cards = $selection;
+    } catch (Exception $e) {
+        echo "Not a number!";
+        die();
+    }
+}
+
+$file->cards = $cards;
+
+echo json_encode($file);
+
+?>
