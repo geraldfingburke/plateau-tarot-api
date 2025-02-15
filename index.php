95b11146ef3b66522410165fd5a4c1787d32021a
geraldfingburke <geraldfingburke@gmail.com>
2025-02-15 11:51:46 -0500

    Add content type to header

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=utf-8");
 $file = json_decode(file_get_contents("json/tarot_card_data.json"));
 
 $cards = $file->cards;
